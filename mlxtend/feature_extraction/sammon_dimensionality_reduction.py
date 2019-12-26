03d793c3511e6b83eba5b5d0f52032db900617df
Deepan Das <ddas@scortex.io>
2019-12-26 17:31:39 +0100

    Initial script for sammon dimensionality reduction

---
+++ mlxtend/feature_extraction/sammon_dimensionality_reduction.py
@@ -0,0 +1,32 @@
+# Sammon Dimensionality Reduction
+
+import numpy as np
+from .._base import _BaseModel
+
+class SammonDimensionalityReduction(_BaseModel):
+    """
+    Sammon Dimensionality Reduction Class
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+    
+    Examples
+    --------
+
+    """
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        pass
+
+    def _fit(self, X):
+        pass
+
+    def transform(self, X):
+        pass
+
+
